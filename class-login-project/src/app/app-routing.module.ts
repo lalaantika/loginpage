c38a4598c51341cce11beecd172a412864ac644a
burningman2k20 <burningman2k9@gmail.com>
2020-04-27 16:22:46 -0400

    added route

--- class-login-project/src/app/app-routing.module.ts
+++ class-login-project/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LogInComponent} from './log-in/log-in.component';
-
+import {LogOutComponent} from './log-out/log-out.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component:  LogInComponent
   },
+  {
+    path: 'logout',
+    component:  LogOutComponent
+  },
+
 ];
 
 @NgModule({
