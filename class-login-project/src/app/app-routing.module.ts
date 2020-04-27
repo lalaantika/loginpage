f4f040a911b8cd1be4ff1f5a37cb8eebe3ebfc60
burningman2k20 <burningman2k9@gmail.com>
2020-04-27 16:26:14 -0400

    updated routing

--- class-login-project/src/app/app-routing.module.ts
+++ class-login-project/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LogInComponent} from './log-in/log-in.component';
 import {LogOutComponent} from './log-out/log-out.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,11 @@ const routes: Routes = [
     path: 'logout',
     component:  LogOutComponent
   },
+  {
+    path: 'signup',
+    component:  SignUpComponent
+  },
+
 
 ];
 
