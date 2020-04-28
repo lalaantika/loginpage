23629b81426912e7fe21ca7efd3a11f8d79b0f90
mojtabafazeli <fazeli.mojtaba@gmail.com>
2020-04-27 22:07:00 -0400

    fixed signup link

--- class-login-project/src/app/app-routing.module.ts
+++ class-login-project/src/app/app-routing.module.ts
@@ -1,13 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LogInComponent} from './log-in/log-in.component';
+import { LogOutComponent } from './log-out/log-out.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   {
-    path: 'login',
-    component:  LogInComponent
+    path: 'logout',
+    component:  LogOutComponent
   },
+
+  {
+    path: 'home',
+    component:  HomeComponent
+  },
+
+  {
+    path: '',
+    redirectTo: '/home', 
+    pathMatch: 'full'
+  },
+
+  {
+    path: '**',
+    component: HomeComponent
+  }
+  
 ];
 
 @NgModule({
